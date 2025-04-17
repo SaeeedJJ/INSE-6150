8be650e860b7bacbfcbcbeb830533c7ed3811d41
SaeeedJJ <saeed.jalalvand2@gmail.com>
2025-04-17 19:32:59 -0400

    Add files via upload

---
+++ InsecureVault.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSL-1.0 (Boost Software License 1.0)
+pragma solidity 0.8.17;
+
+import "./Dependencies.sol";
+
+// InsecureMoonVault must be the contract owner of the MoonToken
+contract InsecureMoonVault is ReentrancyGuard {
+    IMoonToken public immutable moonToken;
+
+    constructor(IMoonToken _moonToken) {
+        moonToken = _moonToken;
+    }
+
+    function deposit() external payable noReentrant {  
+        bool success = moonToken.mint(msg.sender, msg.value);
+        require(success, "Failed to mint token");
+    }
+
+    function withdrawAll() external noReentrant {  
+        uint256 balance = getUserBalance(msg.sender);
+        require(balance > 0, "Insufficient balance");
+
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Failed to send Ether");
+
+        success = moonToken.burnAccount(msg.sender);
+        require(success, "Failed to burn token");
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getUserBalance(address _user) public view returns (uint256) {
+        return moonToken.balanceOf(_user);
+    }
+}
